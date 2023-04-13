70cdfe5a8c8527270fbb3148aa2dce617e2e76ac
Martin Pastoors <martin.pastoors@gmail.com>
2023-04-13 09:13:31 +0200

    Auto update

--- R/auto.r
+++ R/auto.r
@@ -7,37 +7,106 @@
 library(tidyverse)
 library(ggthemes)
 library(readxl)
-# install.packages("googledrive")
-library(googledrive)
+library(googledrive)   # install.packages("googledrive")
+library(ggrepel)
 
 options(dplyr.summarise.inform = FALSE)
 
 source("r/my utils.r")
 
-# set path
-my.filepath <- file.path(get_dropbox(),"Hardloop")
-
 # drive_find()
 drive_download(file="Auto verbruik.xlsx", overwrite=TRUE)
-t <- 
-  readxl::read_excel("Auto verbruik.xlsx") %>% 
+
+t <-
+  readxl::read_excel("C:/Users/MartinPastoors/Dropbox/Martin en Pina/auto verbruik.xlsx") %>% 
   lowcase() %>% 
+  drop_na(datum) %>% 
+  drop_na(kmafgelegd) %>% 
+  filter(auto != "Corsa 2021 blauw") %>% 
+  dplyr::select(c(1:5,15)) %>% 
+  
+  # bind google drive document
+  bind_rows(
+    readxl::read_excel("Auto verbruik.xlsx") %>% 
+    lowcase() %>% 
+    drop_na(datum) %>% 
+    drop_na(kmafgelegd) %>% 
+    dplyr::select(c(1:5,11))    
+  ) %>% 
   mutate(
     yday  = lubridate::yday(datum),
     month = lubridate::month(datum),
-    year  = lubridate::year(datum)
+    year  = lubridate::year(datum),
+    decade= 10*floor(year/10)
   ) %>% 
-  drop_na(datum) %>% 
-  mutate(kmafgelegd = ifelse(is.na(kmafgelegd), 0, kmafgelegd)) %>% 
+  arrange(datum) %>% 
+  
+  # quick fix for missing liters or euros
+  mutate(
+    litersgetankt = ifelse(is.na(litersgetankt), lag(litersgetankt) * kmafgelegd/lag(kmafgelegd), litersgetankt),
+    eurogetankt   = ifelse(is.na(eurogetankt), lag(eurogetankt) * kmafgelegd/lag(kmafgelegd), eurogetankt)
+  ) %>% 
+  
   group_by(year) %>% 
-  mutate(km = cumsum(kmafgelegd))
+  mutate(
+    km   = cumsum(kmafgelegd),
+    euro =cumsum(eurogetankt),
+    liters=cumsum(litersgetankt)
+  ) %>% 
+  filter(year >= 2003) %>% 
+  filter(year %notin% 2014:2018)  # missing Corsa Eco data
+  
 
+tt <-
+  t %>% 
+  group_by(year) %>% 
+  slice_tail(n=1)
+
+# plot km
 t %>% 
-  filter(year >= 2022) %>% 
   ggplot(aes(x=yday, y=km, group=year)) +
   theme_publication() +
+  theme(legend.position="none") +
   geom_line(aes(colour=as.character(year))) +
-  scale_x_continuous(breaks=seq(0,365,50))
+  geom_point(data=tt,
+             aes(colour=as.character(year))) +
+  scale_x_continuous(limits=c(0,400), breaks=seq(0,350,50)) +
+  ggrepel::geom_text_repel(data=tt, 
+                           aes(label=year, colour=as.character(year)),
+                           hjust=0, size=3, segment.size=0.25, segment.linetype="dashed", nudge_x=5, direction="y",
+                           min.segment.length = 0) +
+  labs(title="afgelegde kilometers") +
+  facet_wrap(~decade)
+
+# plot euro
+t %>% 
+  ggplot(aes(x=yday, y=euro, group=year)) +
+  theme_publication() +
+  theme(legend.position="none") +
+  geom_line(aes(colour=as.character(year))) +
+  geom_point(data=tt,
+             aes(colour=as.character(year))) +
+  scale_x_continuous(limits=c(0,400), breaks=seq(0,350,50)) +
+  ggrepel::geom_text_repel(data=tt, 
+                           aes(label=year, colour=as.character(year)),
+                           hjust=0, size=3, segment.size=0.25, segment.linetype="dashed", nudge_x=5, direction="y",
+                           min.segment.length = 0) +
+  facet_wrap(~decade)
+
+# plot liters
+t %>% 
+  ggplot(aes(x=yday, y=liters, group=year)) +
+  theme_publication() +
+  theme(legend.position="none") +
+  geom_line(aes(colour=as.character(year))) +
+  geom_point(data=tt,
+             aes(colour=as.character(year))) +
+  scale_x_continuous(limits=c(0,400), breaks=seq(0,350,50)) +
+  ggrepel::geom_text_repel(data=tt, 
+                           aes(label=year, colour=as.character(year)),
+                           hjust=0, size=3, segment.size=0.25, segment.linetype="dashed", nudge_x=5, direction="y",
+                           min.segment.length = 0) +
+  facet_wrap(~decade)
 
 skimr::skim(t)
 count_not_finite(t)
